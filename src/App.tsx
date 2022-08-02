00f63d7e03841112f49783466fbf3a3e4493c171
Igor Belikov <igorbelikov2009@yandex.ru>
2022-08-02 18:35:25 +0300

    ModalContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Product from "./component/Product";
+import { useProducts } from "./hooks/useProducts";
+import Loader from "./component/Loader";
+import ErrorMessage from "./component/ErrorMessage";
+import Modal from "./component/Modal";
+import CreateProduct from "./component/CreateProduct";
+import { useContext } from "react";
+import { IProduct } from "./models";
+import { ModalContext } from "./context/ModalContext";
 
 function App() {
+  const { loading, error, products, addProduct } = useProducts();
+  const { modal, open, close } = useContext(ModalContext);
+
+  const createHandler = (product: IProduct) => {
+    close();
+    addProduct(product);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+
+      {modal && (
+        <Modal title="Create new product" onClose={close}>
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
+
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+        onClick={open}
+      >
+        +
+      </button>
     </div>
   );
 }
---
+++ src/component/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ErrorMessageProps {
+  error: string;
+}
+
+const ErrorMessage = ({ error }: ErrorMessageProps) => {
+  return (
+    <div>
+      <p className="text-center text-red-600"> {error} </p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
